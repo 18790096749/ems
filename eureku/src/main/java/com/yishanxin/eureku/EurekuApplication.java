033f5883366dfbc7740c20405471f2a171ad9ec3
root <18790096749@163.com>
2019-04-22 17:46:16 +0800

    添加第一个项目

---
+++ eureku/src/main/java/com/yishanxin/eureku/EurekuApplication.java
@@ -0,0 +1,15 @@
+package com.yishanxin.eureku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekuApplication.class, args);
+    }
+
+}
